1fc3a79a06eae2cc8741d36f8378cc639bcd9741
amanjit805 <amanjit805@gmail.com>
2026-02-09 21:23:01 +0000

    change made in Lab01_Java.java in codespaces

--- Lab Folders/Lab01/Lab01_Java.java
+++ Lab Folders/Lab01/Lab01_Java.java
@@ -5,6 +5,7 @@ void testMethod1()
   //First command x = y + z
   //Second commnad print(x)
   // Edit made by AmanjitSingh_240120382.
+  // Edit made by AmanjitSingh_240120382 in Codespaces
 
   //Last line print('Good bye')
 }
